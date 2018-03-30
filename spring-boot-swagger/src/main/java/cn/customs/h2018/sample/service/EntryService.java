b503e88aa2cb4dee871c5ebbb76962d0bd159557
lizhipeng <lizhipeng@gmail.com>
2018-03-30 12:38:14 +0800

    delete

--- spring-boot-swagger/src/main/java/cn/customs/h2018/sample/service/EntryService.java
+++ spring-boot-swagger/src/main/java/cn/customs/h2018/sample/service/EntryService.java
@@ -1,7 +1,8 @@
-/**  
- * Date: 2018-03-23 15:10:33. 
+/**
+ * Date: 2018-03-23 15:10:33.
+ * 
  * @author: lizhipeng.
- */  
+ */
 package cn.customs.h2018.sample.service;
 
 import java.util.List;
@@ -16,45 +17,57 @@ import cn.customs.h2018.sample.pojo.EntryList;
 import lombok.extern.slf4j.Slf4j;
 
 
-/**  
- * Date: 2018-03-23 15:10:33. 
+/**
+ * Date: 2018-03-23 15:10:33.
+ * 
  * @author: lizhipeng.
- * @description: 
+ * @description:
  */
 @Service
 @Transactional
 @Slf4j
 public class EntryService {
-  
+
   private EntryHeadRepository repoHead;
   private EntryListRepository repoList;
-  
+
   @Autowired
-  EntryService(EntryHeadRepository repoHead, EntryListRepository repoList){
+  EntryService(EntryHeadRepository repoHead, EntryListRepository repoList) {
     this.repoHead = repoHead;
     this.repoList = repoList;
   }
 
-  
-  public Entry save(Entry entry){
-    
+
+  /**
+   * 保存报关单 
+   *
+   * Date: 2018-03-30 11:20:34 
+   * @author lizhipeng 
+   *
+   * @param entry
+   * @return
+   */
+  public Entry save(Entry entry) {
+
     EntryHead head = entry.getEntryHead();
     List<EntryList> lists = entry.getEntryList();
     
+    log.info("old:{}", entry);
+
     head = repoHead.save(head);
     lists = repoList.saveAll(lists);
-    
-    return entry;
-    
+
+    return Entry.builder().entryHead(head).entryList(lists).build();
+
   }
-  
-  public Entry getById(String entryId){
-    
+
+  public Entry getById(String entryId) {
+
     EntryHead head = repoHead.findById(entryId).orElse(null);
     List<EntryList> lists = repoList.findByEntryId(entryId);
-    
+
     return Entry.builder().entryHead(head).entryList(lists).build();
-    
+
   }
 }
-  
+
--- spring-boot-swagger/src/test/java/cn/customs/h2018/sample/controller/BussinessControllerTests.java
+++ spring-boot-swagger/src/test/java/cn/customs/h2018/sample/controller/BussinessControllerTests.java
@@ -7,7 +7,6 @@ package cn.customs.h2018.sample.controller;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import lombok.extern.slf4j.Slf4j;
 
 /**  
  * Date: 2018-03-23 15:33:20. 
@@ -16,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Slf4j
 public class BussinessControllerTests {
 
 }
